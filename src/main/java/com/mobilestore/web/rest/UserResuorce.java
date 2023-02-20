87fb166658e92bb0a42fca7cbb8f7386106f9f6a
trichoip <107303342+trichoip@users.noreply.github.com>
2023-02-21 00:56:26 +0700

    Revert "up"
    This reverts commit 64adaea26858d8d20276166915fbd7b79d112cfb.

---
+++ src/main/java/com/mobilestore/web/rest/UserResuorce.java
@@ -0,0 +1,47 @@
+package com.mobilestore.web.rest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mobilestore.service.dto.JWTToken;
+import com.mobilestore.service.dto.LoginDTO;
+import com.mobilestore.util.JwtUtils;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+public class UserResuorce {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtUtils jwtUtils;
+
+    public UserResuorce(AuthenticationManagerBuilder authenticationManagerBuilder, JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JWTToken> authorize(@RequestBody LoginDTO login) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                login.getUsernameOrEmail(),
+                login.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt);
+        return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+    }
+}
