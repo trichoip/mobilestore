53d61e66537890f73da544de2242c619782eb8e0
trichoip <107303342+trichoip@users.noreply.github.com>
2023-02-21 00:15:04 +0700

    y

--- src/main/java/com/mobilestore/web/rest/CartResource.java
+++ src/main/java/com/mobilestore/web/rest/CartResource.java
@@ -8,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.mobilestore.domain.Order;
-import com.mobilestore.domain.Product;
 import com.mobilestore.repository.OrderRepository;
 import com.mobilestore.service.OrderService;
 import com.mobilestore.service.ProductService;
--- src/main/java/com/mobilestore/web/rest/UserResuorce.java
+++ src/main/java/com/mobilestore/web/rest/UserResuorce.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
