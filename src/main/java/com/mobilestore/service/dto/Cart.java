c20530fc9aab7dfad4a274619bcca82810a85ba0
trichoip <107303342+trichoip@users.noreply.github.com>
2023-02-21 10:31:57 +0700

    pp

--- src/main/java/com/mobilestore/service/dto/Cart.java
+++ src/main/java/com/mobilestore/service/dto/Cart.java
@@ -7,117 +7,117 @@ import java.util.Map;
 
 public class Cart {
 
-    private Map<Long, ProductDTO> cart;
-    private Collection<ProductDTO> cartList;
+    // private Map<Long, ProductDTO> cart;
+    // private Collection<ProductDTO> cartList;
 
-    public Cart() {
-        this.totalMoney = getTotalMoney();
-        this.totalQuantity = getTotalQuantity();
-        this.cartList = getCartList();
-    }
+    // public Cart() {
+    // this.totalMoney = getTotalMoney();
+    // this.totalQuantity = getTotalQuantity();
+    // this.cartList = getCartList();
+    // }
 
-    public Collection<ProductDTO> getCartList() {
-        if (cart != null) {
-            return cart.values();
-        }
-        return cartList;
-    }
+    // public Collection<ProductDTO> getCartList() {
+    // if (cart != null) {
+    // return cart.values();
+    // }
+    // return cartList;
+    // }
 
-    public void setCartList(Collection<ProductDTO> cartList) {
-        this.cartList = cartList;
-    }
+    // public void setCartList(Collection<ProductDTO> cartList) {
+    // this.cartList = cartList;
+    // }
 
-    private Double totalMoney;
-    private Integer totalQuantity;
+    // private Double totalMoney;
+    // private Integer totalQuantity;
 
-    public Integer getTotalQuantity() {
-        Integer total = 0;
-        if (this.cart != null) {
-            for (ProductDTO item : cart.values()) {
-                total += item.getQuantity();
-            }
-        }
-        return total;
-    }
+    // public Integer getTotalQuantity() {
+    // Integer total = 0;
+    // if (this.cart != null) {
+    // for (ProductDTO item : cart.values()) {
+    // total += item.getQuantity();
+    // }
+    // }
+    // return total;
+    // }
 
-    public void setTotalMoney(Double totalMoney) {
-        this.totalMoney = totalMoney;
-    }
+    // public void setTotalMoney(Double totalMoney) {
+    // this.totalMoney = totalMoney;
+    // }
 
-    public Cart(Map<Long, ProductDTO> cart) {
-        this.cart = cart;
-    }
+    // public Cart(Map<Long, ProductDTO> cart) {
+    // this.cart = cart;
+    // }
 
-    public Map<Long, ProductDTO> getCart() {
-        return cart;
-    }
+    // public Map<Long, ProductDTO> getCart() {
+    // return cart;
+    // }
 
-    public void setCart(Map<Long, ProductDTO> cart) {
-        this.cart = cart;
-    }
+    // public void setCart(Map<Long, ProductDTO> cart) {
+    // this.cart = cart;
+    // }
 
-    public boolean add(ProductDTO product) {
-        boolean result = false;
-        if (this.cart == null) {
-            this.cart = new HashMap<>();
-        }
-        if (this.cart.containsKey(product.getId())) {
-            int currentQuantity = this.cart.get(product.getId()).getQuantity();
-            product.setQuantity(currentQuantity + product.getQuantity());
+    // public boolean add(ProductDTO product) {
+    // boolean result = false;
+    // if (this.cart == null) {
+    // this.cart = new HashMap<>();
+    // }
+    // if (this.cart.containsKey(product.getId())) {
+    // int currentQuantity = this.cart.get(product.getId()).getQuantity();
+    // product.setQuantity(currentQuantity + product.getQuantity());
 
-        }
-        this.cart.put(product.getId(), product);
-        result = true;
-        return result;
-    }
+    // }
+    // this.cart.put(product.getId(), product);
+    // result = true;
+    // return result;
+    // }
 
-    public boolean update(Long id, ProductDTO product) {
-        boolean result = false;
-        if (this.cart != null) {
-            if (this.cart.containsKey(id)) {
-                this.cart.replace(id, product);
-                result = true;
-            }
-        }
+    // public boolean update(Long id, ProductDTO product) {
+    // boolean result = false;
+    // if (this.cart != null) {
+    // if (this.cart.containsKey(id)) {
+    // this.cart.replace(id, product);
+    // result = true;
+    // }
+    // }
 
-        return result;
-    }
+    // return result;
+    // }
 
-    public boolean remove(Long id) {
-        boolean result = false;
-        if (this.cart != null) {
-            if (this.cart.containsKey(id)) {
-                this.cart.remove(id);
-                result = true;
-            }
-        }
+    // public boolean remove(Long id) {
+    // boolean result = false;
+    // if (this.cart != null) {
+    // if (this.cart.containsKey(id)) {
+    // this.cart.remove(id);
+    // result = true;
+    // }
+    // }
 
-        return result;
-    }
+    // return result;
+    // }
 
-    public boolean checkProduct(Long id) {
-        boolean result = false;
-        if (this.cart != null) {
-            if (this.cart.containsKey(id)) {
-                result = true;
-            }
-        }
+    // public boolean checkProduct(Long id) {
+    // boolean result = false;
+    // if (this.cart != null) {
+    // if (this.cart.containsKey(id)) {
+    // result = true;
+    // }
+    // }
 
-        return result;
-    }
+    // return result;
+    // }
 
-    public Double getTotalMoney() {
-        Double total = 0.0;
-        if (this.cart != null) {
-            for (ProductDTO item : cart.values()) {
-                total += item.getPrice() * item.getQuantity();
-            }
-        }
-        return total;
-    }
+    // public Double getTotalMoney() {
+    // Double total = 0.0;
+    // if (this.cart != null) {
+    // for (ProductDTO item : cart.values()) {
+    // total += item.getPrice() * item.getQuantity();
+    // }
+    // }
+    // return total;
+    // }
 
-    public void setTotalQuantity(Integer totalQuantity) {
-        this.totalQuantity = totalQuantity;
-    }
+    // public void setTotalQuantity(Integer totalQuantity) {
+    // this.totalQuantity = totalQuantity;
+    // }
 
 }
